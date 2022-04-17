4e819fd46f1c4572d353e16aea5d056966cfbb4e
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-04-17 19:22:35 -0400

    configuramos variables de entorno para la base de datos

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,8 @@ dotenv.config();
 
 export const config = {
   APP_PORT: process.env.APP_PORT || "3000",
+
+  DATABASE_HOST: process.env.DATABASE_HOST || "localhost",
+  DATABASE_PORT: process.env.DATABASE_PORT || "27017",
+  DATABASE_NAME: process.env.DATABASE_NAME || "example",
 };
-
-
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,12 @@
 import { connect } from "mongoose";
 
+import { config } from "../config";
+
 (async () => {
   try {
-    await connect("mongodb://localhost:27017/docker");
+    await connect(
+      `mongodb://${config.DATABASE_HOST}:${config.DATABASE_PORT}/${config.DATABASE_NAME}`
+    );
     console.log("Database connected");
   } catch (error) {
     console.log("Error to connect the database");
